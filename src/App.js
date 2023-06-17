a5a0362b40879a326044626061a6ce4e82230cf1
Ivan <ivananatolevic66@gmail.com>
2023-06-17 18:49:00 +0300

    version 0.5

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import React, { useEffect, useState } from 'react';
 import Todo from './components/Todo';
 function App() {
 	const [todo, setTodo] = useState([]);
+	const [post, setPost] = useState('');
 	useEffect(() => {
-		axios
-			.get('https://jsonplaceholder.typicode.com/todos')
-			.then((res) => setTodo(res.data));
+		axios.get('http://localhost:3004/todos').then((res) => setTodo(res.data));
 	}, []);
+
 	return (
 		<div className="App">
 			<div className="container">
-			<Todo todo={todo} />
+				<Todo todo={todo} post={post} setPost={setPost} setTodo={setTodo} />
 			</div>
 		</div>
 	);
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,21 +1,55 @@
-import React from 'react';
+// import React, { useState } from 'react';
 import styles from './Todo.module.scss';
-const Todo = ({ todo }) => {
+import axios from 'axios';
+const Todo = ({ todo, setTodo, post, setPost }) => {
+	// const [isChecked, setIsChecked] = useState(false);
+
+	// const handleClick = () => {
+	// 	setIsChecked(!isChecked);
+	// };
+	const handleChange = ({ target }) => {
+		setPost(target.value);
+	};
+
+	const addNewPost = () => {
+		const newPost = {
+			id: Math.floor(Math.random() * (100 - 1 + 1) + 1),
+			title: post,
+		};
+		axios.post('http://localhost:3004/todos', newPost);
+		setTodo((prev) => [...prev, newPost]);
+		console.log(newPost);
+	};
+
+	const deletePost = (id) => {
+		axios.delete(`http://localhost:3004/todos/${id}`);
+		setTodo((prev) => prev.filter((filt) => filt.id !== id));
+	};
+
 	return (
 		<div className={styles.todo_app}>
 			<h1>To-Do List</h1>
+			<div className={styles.row}>
+				<input
+					type="text"
+					value={post}
+					placeholder="Add your text"
+					onChange={handleChange}
+				/>
+				<button onClick={addNewPost}>Add</button>
+			</div>
 			<ul>
-				{todo.map((item) =>
-					item.completed ? (
-						<li key={item.id} id={item.id} className={styles.checkmark}>
-							{item.title}
-						</li>
-					) : (
-						<li key={item.id} id={item.id}>
-							{item.title}
-						</li>
-					),
-				)}
+				{todo.map((item) => (
+					<li key={item.id} id={item.id}>
+						{/* <img
+							className='checkbox'
+							src={isChecked ? '/images/checked.png' : '/images/unchecked.png'}
+							onClick={handleClick}
+							alt='checkbox'
+						/> */}
+						{item.id}. {item.title} <span onClick={() => deletePost(item.id)}>✕</span>
+					</li>
+				))}
 			</ul>
 		</div>
 	);
